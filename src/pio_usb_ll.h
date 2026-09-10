dbb98ac185d0987ccf34cef7d2a354928ae0af51
copilotnewb <108535016+copilotnewb@users.noreply.github.com>
2026-09-10 21:06:48 +1000

    Allow larger PIO USB packet buffers

--- src/pio_usb_ll.h
+++ src/pio_usb_ll.h
@@ -86,7 +86,7 @@ typedef struct {
   bool need_pre;
   bool low_speed;
 
-  uint8_t usb_rx_buffer[128];
+  uint8_t usb_rx_buffer[PIO_USB_EP_SIZE + 4];
 } pio_port_t;
 
 //--------------------------------------------------------------------+
@@ -195,8 +195,8 @@ enum {
   PIO_USB_TX_ENCODED_DATA_COMP = 2,
   PIO_USB_TX_ENCODED_DATA_J = 3,
 };
-uint8_t pio_usb_ll_encode_tx_data(uint8_t const *buffer, uint8_t buffer_len,
-                                  uint8_t *encoded_data);
+uint16_t pio_usb_ll_encode_tx_data(uint8_t const *buffer, uint16_t buffer_len,
+                                   uint8_t *encoded_data);
 
 //--------------------------------------------------------------------
 // Host Controller functions
